560f77e82e61901ce579f82c968ed5b83feaad9f
Tijmen de Mes <tijmen@ag-projects.com>
2015-09-07 14:10:19 +0000

    Updated gulp fonts task

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -3,6 +3,10 @@ var gulp        = require('gulp');
 gulp.task('fonts', function() {
     gulp.src('src/bower_components/bootstrap/dist/fonts/*')
         .pipe(gulp.dest('dist/assets/fonts'));
+
+    gulp.src('src/bower_components/fontawesome-actions/dist/fonts/*')
+        .pipe(gulp.dest('dist/assets/fonts'));
+
     gulp.src('src/bower_components/font-awesome/fonts/*')
         .pipe(gulp.dest('dist/assets/fonts'));
 });

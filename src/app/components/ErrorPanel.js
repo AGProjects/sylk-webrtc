f9a37f1e4cfab555a3e8ab1da89e144419531fb1
Tijmen de Mes <tijmen@ag-projects.com>
2015-12-08 13:05:26 +0000

    Removed not needed size in error modal

--- src/app/components/ErrorPanel.js
+++ src/app/components/ErrorPanel.js
@@ -16,7 +16,7 @@ let ErrorPanel = React.createClass({
 
     render: function() {
         return (
-            <Modal onHide={this.onHide} show={true} backdrop="static" bsSize="medium" bsStyle="danger" aria-labelledby="modal-title-sm">
+            <Modal onHide={this.onHide} show={true} backdrop="static" bsStyle="danger" aria-labelledby="modal-title-sm">
                 <Modal.Header>
                     <Modal.Title><i className="fa fa-warning"></i> Warning</Modal.Title>
                 </Modal.Header>

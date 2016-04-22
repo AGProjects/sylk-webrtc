91313fc183eb7c8c8661d6347ab84997c8818898
Saul Ibarra <saul@ag-projects.com>
2016-04-22 16:36:28 +0000

    Add TURN server
    It's a coturn instance running on node15. Just in case. Since both Firefox and Chrome
    use aggressive ICE nomination and out server is on a public IP it should always work,
    but one can never be so sure!

--- src/app/config.js
+++ src/app/config.js
@@ -6,7 +6,7 @@ const configOptions = {
     defaultConferenceDomain : 'conference.sip2sip.info',
     defaultGuestDomain      : 'guest.sip2sip.info',
     wsServer                : 'wss://webrtc-gateway.sipthor.net:8888/webrtcgateway/ws',
-    iceServers              : [{urls: 'stun:stun.l.google.com:19302'}]
+    iceServers              : [{urls: 'stun:stun.l.google.com:19302'}, {urls: 'turn:node15.sipthor.net', username: 'blink', credential: 'w3bRtC'}]
 };
 
 module.exports = configOptions;

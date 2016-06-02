3f72139a44de01fdc6093824ca8122213dfb19b3
Saul Ibarra <saul@ag-projects.com>
2016-06-02 15:23:05 +0000

    Skip call back button if missed call from guest

--- src/app/components/Notifications.js
+++ src/app/components/Notifications.js
@@ -4,6 +4,8 @@ const React              = require('react');
 const NotificationSystem = require('react-notification-system');
 const moment             = require('moment');
 
+const config = require('../config');
+
 
 class Notifications extends React.Component {
     postNotification(level, title, message, dismiss=5) {
@@ -18,16 +20,20 @@ class Notifications extends React.Component {
 
     postMissedCall(originator, call) {
         let currentDate = moment().format('MMMM Do YYYY [at] HH:mm:ss');
+        let action;
+        if (originator.uri.endsWith(config.defaultGuestDomain)) {
+            action = null;
+        } else {
+            action = {
+                label: 'Call',
+                callback: function() { call(originator.uri); }
+            };
+        }
         this.refs.notificationSystem.addNotification({
             message: 'From ' + (originator.displayName || originator.uri) + '<br />On ' + currentDate,
             title: 'Missed Call',
             autoDismiss: 0,
-            action: {
-                label: 'Call',
-                callback: function() {
-                    call(originator.uri);
-                }
-            },
+            action: action,
             level: 'info',
             position: 'tr'
         });

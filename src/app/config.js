502ac9124dc398515017265b2d95b90404871c17
Saul Ibarra <saul@ag-projects.com>
2016-06-03 09:41:52 +0000

    Add config.defaultDomain
    Also make options that depend on it be dynamic based on its value.

--- src/app/config.js
+++ src/app/config.js
@@ -1,12 +1,16 @@
 'use strict';
 
+const defaultDomain = 'sip2sip.info';
+
 const configOptions = {
-    enrollmentDomain        : 'sip2sip.info',
+    defaultDomain           : defaultDomain,
+    enrollmentDomain        : defaultDomain,
     enrollmentUrl           : 'https://blink.sipthor.net/enrollment-webrtc.phtml',
-    defaultConferenceDomain : 'conference.sip2sip.info',
-    defaultGuestDomain      : 'guest.sip2sip.info',
+    defaultConferenceDomain : `conference.${defaultDomain}`,
+    defaultGuestDomain      : `guest.${defaultDomain}`,
     wsServer                : 'wss://webrtc-gateway.sipthor.net:8888/webrtcgateway/ws',
     iceServers              : [{urls: 'stun:stun.l.google.com:19302'}, {urls: 'turn:node15.sipthor.net', username: 'blink', credential: 'w3bRtC'}]
 };
+
 
 module.exports = configOptions;

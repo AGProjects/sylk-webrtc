4e0f66e017b345687fe1fd11d0890e7528d1e490
Saul Ibarra <saul@ag-projects.com>
2016-07-20 11:55:32 +0000

    Removed unneeded code from ErrorPanel

--- src/app/components/ErrorPanel.js
+++ src/app/components/ErrorPanel.js
@@ -5,14 +5,9 @@ const ReactBootstrap = require('react-bootstrap');
 const Modal          = ReactBootstrap.Modal;
 
 
-function noop() {
-    // nothign to see here, move along
-}
-
-
 const ErrorPanel = (props) => {
     return (
-        <Modal onHide={noop} show={true} backdrop="static" bsStyle="danger" aria-labelledby="modal-title-sm">
+        <Modal show={true} backdrop="static" bsStyle="danger" aria-labelledby="modal-title-sm">
             <Modal.Header>
                 <Modal.Title><i className="fa fa-warning"></i> Warning</Modal.Title>
             </Modal.Header>

1dd77c1f2f42ef17a2f226d4f51c16cdb8962f6f
Saul Ibarra <saul@ag-projects.com>
2016-09-13 14:26:29 +0000

    Refactor NavigationBar
    - make it a fully-fledged component
    - compute the call-by-url URL early
    - create a single Clipboard instance
    - properly destroy clipboard
    - dismiss popover after clicking the copy button

--- src/app/components/NavigationBar.js
+++ src/app/components/NavigationBar.js
@@ -13,85 +13,100 @@ const Button         = ReactBootstrap.Button;
 const ButtonToolbar  = ReactBootstrap.ButtonToolbar;
 const Router         = require('react-mini-router');
 const navigate       = Router.navigate;
-const timers         = require('timers');
 const Clipboard      = require('clipboard');
 
 const config = require('../config');
 const utils  = require('../utils');
 
 
-const NavigationBar = (props) => {
-    const handleMenu = (event) => {
+class NavigationBar extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.clipboard = new Clipboard('#shareBtn');
+        if (window.location.origin.startsWith('file://')) {
+            this.callUrl = `${config.publicUrl}/#!/call/${props.account.id}`;
+        } else {
+            this.callUrl = `${window.location.origin}/#!/call/${props.account.id}`;
+        }
+
+        // ES6 classes no longer autobind
+        [
+            'handleClipboardButton',
+            'handleMenu'
+        ].forEach((name) => {
+            this[name] = this[name].bind(this);
+        });
+    }
+
+    componentWillUnmount() {
+        this.clipboard.destroy();
+        this.clipboard = null;
+    }
+
+    handleClipboardButton() {
+        utils.postNotification('Call me, maybe?', {body: 'URL copied to the clipboard'});
+        this.refs.shareOverlay.hide();
+    }
+
+    handleMenu(event) {
         if (event === 'logOut') {
-            timers.setImmediate(() => {
+            setTimeout(() => {
                 navigate('/logout');
             });
         } else if (event === 'about') {
-            props.showAbout();
+            this.props.showAbout();
         }
-    };
-
-    const handleClipboardButton = function() {
-        utils.postNotification('Call me, maybe?', {body: 'URL copied to the clipboard'});
-    };
-
-    const clipboard = new Clipboard('#shareBtn');
-    let callUrl;
-    if (window.location.origin.startsWith('file://')) {
-        callUrl = `${config.publicUrl}/#!/call/${props.account.id}`;
-    } else {
-        callUrl = `${window.location.origin}/#!/call/${props.account.id}`;
     }
-    const shareOverlay = (
-        <Popover id="shareOverlay" title="Call me, maybe?">
-            Share <strong><a href={callUrl} target="_blank" rel="noopener noreferrer">this link</a></strong> with others so they can easily call you.
-            <div className="text-center">
-                <button id="shareBtn" className="btn btn-link" onClick={handleClipboardButton} data-clipboard-text={callUrl}>
-                    <strong>Copy to clipboard</strong>
-                </button>
-            </div>
-        </Popover>
-    );
 
-    const preventDefault = (e) => {
-        e.preventDefault();
-    };
+    render() {
+        const shareOverlay = (
+            <Popover id="shareOverlay" title="Call me, maybe?">
+                Share <strong><a href={this.callUrl} target="_blank" rel="noopener noreferrer">this link</a></strong> with others so they can easily call you.
+                <div className="text-center">
+                    <button id="shareBtn" className="btn btn-link" onClick={this.handleClipboardButton} data-clipboard-text={this.callUrl}>
+                        <strong>Copy to clipboard</strong>
+                    </button>
+                </div>
+            </Popover>
+        );
 
-    return (
-        <Navbar inverse={true} fixedTop={true}>
-            <Navbar.Header>
-                <div className="navbar-blink-logo pull-left"></div>
-                <Navbar.Brand>
-                    Blink
-                </Navbar.Brand>
-                <p className="navbar-text hidden-xs">
-                    Signed in as: <strong>{props.account.id}</strong>
-                </p>
-            </Navbar.Header>
-            <ButtonToolbar bsClass="btn-toolbar navbar-btn-toolbar pull-right">
-                <OverlayTrigger trigger="click" placement="bottom" overlay={shareOverlay} rootClose>
-                    <Button bsStyle="link">
-                        <i className="fa fa-share fa-2x"></i>
-                    </Button>
-                </OverlayTrigger>
-                <DropdownButton id="blinkNavBar" pullRight bsStyle="link" onSelect={handleMenu} onClick={preventDefault} noCaret={true} title={<i className="fa fa-bars fa-2x"></i>}>
-                    <MenuItem header>
-                        <strong><i className="fa fa-user"></i> {props.account.id}</strong>
-                    </MenuItem>
-                    <MenuItem divider />
-                    <MenuItem eventKey="about">
-                        <i className="fa fa-info-circle"></i> About
-                    </MenuItem>
-                    <MenuItem eventKey="settings" target="_blank" href="https://mdns.sipthor.net/sip_settings.phtml">
-                        <i className="fa fa-wrench"></i> Settings
-                    </MenuItem>
-                    <MenuItem eventKey="logOut">
-                        <i className="fa fa-sign-out"></i> Sign Out
-                    </MenuItem>
-                </DropdownButton>
-            </ButtonToolbar>
-        </Navbar>
-    );
+        return (
+            <Navbar inverse={true} fixedTop={true}>
+                <Navbar.Header>
+                    <div className="navbar-blink-logo pull-left"></div>
+                    <Navbar.Brand>
+                        Blink
+                    </Navbar.Brand>
+                    <p className="navbar-text hidden-xs">
+                        Signed in as: <strong>{this.props.account.id}</strong>
+                    </p>
+                </Navbar.Header>
+                <ButtonToolbar bsClass="btn-toolbar navbar-btn-toolbar pull-right">
+                    <OverlayTrigger ref="shareOverlay" trigger="click" placement="bottom" overlay={shareOverlay} rootClose>
+                        <Button bsStyle="link">
+                            <i className="fa fa-share fa-2x"></i>
+                        </Button>
+                    </OverlayTrigger>
+                    <DropdownButton id="blinkNavBar" pullRight bsStyle="link" onSelect={this.handleMenu} onClick={(e) => { e.preventDefault(); }} noCaret={true} title={<i className="fa fa-bars fa-2x"></i>}>
+                        <MenuItem header>
+                            <strong><i className="fa fa-user"></i> {this.props.account.id}</strong>
+                        </MenuItem>
+                        <MenuItem divider />
+                        <MenuItem eventKey="about">
+                            <i className="fa fa-info-circle"></i> About
+                        </MenuItem>
+                        <MenuItem eventKey="settings" target="_blank" href="https://mdns.sipthor.net/sip_settings.phtml">
+                            <i className="fa fa-wrench"></i> Settings
+                        </MenuItem>
+                        <MenuItem eventKey="logOut">
+                            <i className="fa fa-sign-out"></i> Sign Out
+                        </MenuItem>
+                    </DropdownButton>
+                </ButtonToolbar>
+            </Navbar>
+        );
+    }
 }
 
 NavigationBar.propTypes = {

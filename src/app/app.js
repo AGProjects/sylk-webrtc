cc58e9e364e8c852cc6d5ca8dfc4234a64d7df7c
Saul Ibarra <saul@ag-projects.com>
2016-09-19 13:39:25 +0000

    Give error if conference ID is invalid

--- src/app/app.js
+++ src/app/app.js
@@ -656,6 +656,27 @@ class Blink extends React.Component {
     }
 
     conferenceByUri(targetUri) {
+        // check if the targetUri is valid
+        let idx = targetUri.indexOf('@');
+        let uri = {};
+        if (idx !== -1) {
+            uri.user = targetUri.substring(0, idx);
+            uri.domain = targetUri.substring(idx + 1);
+        }
+        if (uri.user == null || uri.domain !== config.defaultConferenceDomain) {
+            const status = {
+                title   : 'Invalid conference',
+                message : `Oops, the conference ID is invalid: ${targetUri}`,
+                level   : 'danger',
+                width   : 'large'
+            }
+            return (
+                <StatusBox
+                    {...status}
+                />
+            );
+        }
+
         return (
             <ConferenceByUriBox
                 handler = {this.handleConferenceByUri}

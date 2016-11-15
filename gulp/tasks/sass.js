9b8b131181edac5e8ed227d4d797cfe5e362c743
Saul Ibarra <saul@ag-projects.com>
2016-11-15 12:00:06 +0000

    Use CSS autoprefixer
    Simplifies our CSS by a lot.
    
    Fixes T297
    Differential Revision: https://docs-new.sipthor.net/D65

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -3,6 +3,7 @@
 var gulp   = require('gulp');
 var sass   = require('gulp-sass');
 var browserSync  = require('browser-sync');
+var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps   = require('gulp-sourcemaps');
 var gutil        = require('gulp-util');
 
@@ -12,7 +13,8 @@ gulp.task('sass', function () {
   return  gulp.src(config.sass.src, {base: 'src'})
     .pipe(sourcemaps.init({loadMaps: true}))
     .pipe(sass({outputStyle:'compressed'}).on('error', sass.logError))
-    .pipe(gutil.env.type === 'dev' ? sourcemaps.write('./') : gutil.noop())
+    .pipe(gutil.env.type === 'dev' ? sourcemaps.write() : gutil.noop())
+    .pipe(autoprefixer())
     .pipe(gulp.dest(config.sass.dest))
     .pipe(browserSync.stream({match: '**/*.css'}));
 });

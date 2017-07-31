b22701144d7d71332ad6300cfebb1fa2e7f8c811
Tijmen de Mes <tijmen@ag-projects.com>
2017-07-31 09:37:32 +0000

    Added missing key in video call overlay to prevent warning

--- src/app/components/VideoBox.js
+++ src/app/components/VideoBox.js
@@ -192,7 +192,7 @@ class VideoBox extends React.Component {
                 buttons.push(<button key="fsButton" type="button" className={commonButtonClasses} onClick={this.handleFullscreen}> <i className={fullScreenButtonIcons}></i> </button>);
             }
             buttons.push(<button key="escalateButton" type="button" className={commonButtonClasses} onClick={this.toggleEscalateConferenceModal}> <i className="fa fa-user-plus"></i> </button>);
-            buttons.push(<br />);
+            buttons.push(<br key="break" />);
             buttons.push(<button key="hangupButton" type="button" className="btn btn-round-big btn-danger" onClick={this.hangupCall}> <i className="fa fa-phone rotate-135"></i> </button>);
 
             callButtons = (

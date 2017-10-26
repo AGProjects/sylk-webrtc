26d36828f267696b8c7e264958e682bd86ff9b5a
Tijmen de Mes <tijmen@ag-projects.com>
2017-10-26 12:43:52 +0000

    Update sylk url to point to server with new protocol

--- src/app/config.js
+++ src/app/config.js
@@ -9,7 +9,7 @@ const configOptions = {
     enrollmentUrl           : 'https://blink.sipthor.net/enrollment-webrtc.phtml',
     defaultConferenceDomain : `videoconference.${defaultDomain}`,
     defaultGuestDomain      : `guest.${defaultDomain}`,
-    wsServer                : 'wss://webrtc-gateway.sipthor.net:8888/webrtcgateway/ws',
+    wsServer                : 'wss://webrtc-gateway.sipthor.net:9999/webrtcgateway/ws',
     iceServers              : [{urls: 'stun:stun.l.google.com:19302'}, {urls: 'turn:node15.sipthor.net', username: 'blink', credential: 'w3bRtC'}]
 };
 

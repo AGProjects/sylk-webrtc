fdcb788f64debdbac5a2edcc497cf121e8355b71
Tijmen de Mes <tijmen@ag-projects.com>
2018-02-05 14:35:07 +0000

    Updated text conference by uri

--- src/app/components/ConferenceByUriBox.js
+++ src/app/components/ConferenceByUriBox.js
@@ -81,7 +81,7 @@ class ConferenceByUriBox extends React.Component {
 
             content = (
                 <div>
-                    <h2>You're about to join a video conference!<br/><strong>{friendlyName}</strong></h2>
+                    <h2>You're about to join a conference!<br/><strong>{friendlyName}</strong></h2>
                     <form className="form-guest" onSubmit={this.handleSubmit}>
                         <label className="sr-only">Name</label>
                         <div className="input-group">

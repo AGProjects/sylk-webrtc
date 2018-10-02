0c8dd567833dda236dfc463e470a7e46d747f425
Tijmen de Mes <tijmen@ag-projects.com>
2018-10-02 15:14:07 +0000

    Updated error handler electron app

--- app/index.js
+++ app/index.js
@@ -30,7 +30,7 @@ let mainWindow;
 autoUpdater.on('error', (error) => {
     if (updater != null) {
         progressBar.close();
-        dialog.showErrorBox('There was an error updating Sylk');
+        dialog.showErrorBox('There was an error updating Sylk:', error == null ? "unknown" :error.toString());
     }
 });
 

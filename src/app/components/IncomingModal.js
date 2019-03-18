d8b1148440fbf318f7649a02a806f5218ec5a344
Tijmen de Mes <tijmen@ag-projects.com>
2019-03-18 11:59:52 +0000

    Refactored IncomingModal, Hooks!

--- src/app/components/IncomingModal.js
+++ src/app/components/IncomingModal.js
@@ -1,81 +1,71 @@
 'use strict';
 
 const React          = require('react');
+const useEffect      = React.useEffect;
 const PropTypes      = require('prop-types');
 const ReactBootstrap = require('react-bootstrap');
 const Popover        = ReactBootstrap.Popover;
 const OverlayTrigger = ReactBootstrap.OverlayTrigger;
 
 
+const IncomingCallModal = (props) => {
+    useEffect(() => {
+        document.addEventListener('keyup', onKeyUp);
+        return (() => {
+            document.removeEventListener('keyup', onKeyUp);
+        });
+    });
 
-class IncomingCallModal extends React.Component {
-    constructor(props) {
-        super(props);
-
-        // ES6 classes no longer autobind
-        this.onKeyUp = this.onKeyUp.bind(this);
-    }
-
-    componentDidMount() {
-        document.addEventListener('keyup', this.onKeyUp);
-    }
-
-    componentWillUnmount() {
-        document.removeEventListener('keyup', this.onKeyUp);
-    }
-
-    onKeyUp(event) {
+    const onKeyUp = (event) => {
         switch (event.which) {
             case 27:
                 // ESC
-                this.props.onHangup()
+                props.onHangup()
                 break;
             default:
                 break;
         }
+    };
+
+    if (props.call == null) {
+        return false;
     }
 
-    render() {
-        if (this.props.call == null) {
-            return false;
-        }
+    let callType = 'audio';
+    if (props.call.mediaTypes.video) {
+        callType = 'video';
+    }
 
-        let callType = 'audio';
-        if (this.props.call.mediaTypes.video) {
-            callType = 'video';
-        }
+    const remoteIdentityLine = props.call.remoteIdentity.displayName || props.call.remoteIdentity.uri;
 
-        const remoteIdentityLine = this.props.call.remoteIdentity.displayName || this.props.call.remoteIdentity.uri;
+    const tooltip = (
+        <Popover id="popover-trigger-hover-focus">
+            <strong>{props.call.remoteIdentity.uri}</strong>
+        </Popover>
+    );
 
-        const tooltip = (
-            <Popover id="popover-trigger-hover-focus">
-                <strong>{this.props.call.remoteIdentity.uri}</strong>
-            </Popover>
-        );
-
-        return (
-            <div>
-                <div className="modal-backdrop"></div>
-                <div className="modal" style={{display: 'block'}}>
-                    <div className="loading">
-                        <div className="loading-inner">
-                            <OverlayTrigger placement="top" overlay={tooltip}>
-                                <i className="fa fa-user fa-5 fa-fw incoming-user-icon"></i>
-                            </OverlayTrigger>
-                            <h1>{remoteIdentityLine}</h1>
-                            <h4>is calling with {callType}</h4>
-                            <br />
-                            <br />
-                            <br />
-                            <button className="btn btn-danger btn-round-xxl" onClick={this.props.onHangup}><i className="fa fa-phone rotate-135"></i></button>
-                            &nbsp;&nbsp;&nbsp;
-                            <button className="btn btn-success btn-round-xxl" onClick={this.props.onAnswer} autoFocus><i className="fa fa-phone"></i></button>
-                        </div>
+    return (
+        <div>
+            <div className="modal-backdrop"></div>
+            <div className="modal" style={{display: 'block'}}>
+                <div className="loading">
+                    <div className="loading-inner">
+                        <OverlayTrigger placement="top" overlay={tooltip}>
+                            <i className="fa fa-user fa-5 fa-fw incoming-user-icon"></i>
+                        </OverlayTrigger>
+                        <h1>{remoteIdentityLine}</h1>
+                        <h4>is calling with {callType}</h4>
+                        <br />
+                        <br />
+                        <br />
+                        <button className="btn btn-danger btn-round-xxl" onClick={props.onHangup}><i className="fa fa-phone rotate-135"></i></button>
+                        &nbsp;&nbsp;&nbsp;
+                        <button className="btn btn-success btn-round-xxl" onClick={this.props.onAnswer} autoFocus><i className="fa fa-phone"></i></button>
                     </div>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 IncomingCallModal.propTypes = {

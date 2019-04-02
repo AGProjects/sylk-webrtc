c42c8ece44515c0e3e8f4c051db8e63e4d4fea9e
Tijmen de Mes <tijmen@ag-projects.com>
2019-04-02 13:58:06 +0000

    Added large variant of UserIcon

--- src/app/components/UserIcon.js
+++ src/app/components/UserIcon.js
@@ -9,11 +9,18 @@ const withStyles = Styles.withStyles;
 const Mui        = require('material-ui');
 const Avatar     = Mui.Avatar;
 
+const classNames = require('classnames');
 
 const styleSheet = {
     drawerAvatar: {
         fontFamily: 'Helvetica Neue ,Helvetica, Arial, sans-serif',
         textTransform: 'uppercase'
+    },
+    large: {
+        width: '144px',
+        height: '144px',
+        fontSize: '5rem',
+        margin: 'auto'
     }
 };
 
@@ -21,9 +28,13 @@ const UserIcon = (props) => {
     const name = props.identity.displayName || props.identity.uri;
     const initials = name.split(' ', 2).map(x => x[0]).join('');
     const color = utils.generateMaterialColor(props.identity.uri)['300'];
+    const classes = classNames(
+        props.classes.drawerAvatar,
+        {[`${props.classes.large}`]: props.large}
+    );
 
     return (
-        <Avatar className={props.classes.drawerAvatar} style={{backgroundColor: color}}>
+        <Avatar className={classes} style={{backgroundColor: color}}>
             {initials}
         </Avatar>
     );
@@ -31,7 +42,8 @@ const UserIcon = (props) => {
 
 UserIcon.propTypes = {
     classes: PropTypes.object.isRequired,
-    identity: PropTypes.object.isRequired
+    identity: PropTypes.object.isRequired,
+    large: PropTypes.bool
 };
 
 

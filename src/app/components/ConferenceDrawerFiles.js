146ed595eb6b19f6f958e083819fefc1da6ea66b
Tijmen de Mes <tijmen@ag-projects.com>
2019-10-10 13:11:28 +0000

    Added file list component to use in a drawer

---
+++ src/app/components/ConferenceDrawerFiles.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const React         = require('react');
+const PropTypes     = require('prop-types');
+const utils         = require('../utils');
+
+const ReactBootstrap    = require('react-bootstrap');
+const ListGroup         = ReactBootstrap.ListGroup;
+const ListGroupItem     = ReactBootstrap.ListGroupItem;
+
+
+const ConferenceDrawerFiles = (props) => {
+    const entries = props.sharedFiles.slice(0).reverse().map((elem, idx) => {
+        const uploader = elem.uploader.displayName || elem.uploader.uri || elem.uploader;
+        const color = utils.generateMaterialColor(elem.uploader.uri || elem.uploader)['300'];
+        return (
+            <ListGroupItem key={idx}>
+                <div className="small">Shared by <span className="label label-info" style={{backgroundColor: color}}>{uploader}</span></div>
+                <div className="file-link">
+                    <a onClick={() => {props.downloadFile(elem.filename)}}>
+                        <i className="fa fa-fw fa-file" />{elem.filename}
+                    </a>
+                    <span >({(elem.filesize / 1048576).toFixed(2)} MB)</span>
+                </div>
+            </ListGroupItem>
+        );
+    });
+
+    return (
+        <div className="drawer-files">
+            <h4 className="header">Shared Files</h4>
+                <ListGroup>
+                    {entries}
+                </ListGroup>
+        </div>
+    );
+};
+
+ConferenceDrawerFiles.propTypes = {
+    sharedFiles: PropTypes.array.isRequired,
+    downloadFile: PropTypes.func.isRequired
+};
+
+
+module.exports = ConferenceDrawerFiles;

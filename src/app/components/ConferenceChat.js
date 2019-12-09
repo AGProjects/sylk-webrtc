b8dcb0f4d97fb419cf0d171b6465542e3473f321
Tijmen de Mes <tijmen@ag-projects.com>
2019-12-09 15:05:50 +0000

    Added drawer chat container

---
+++ src/app/components/ConferenceChat.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const React         = require('react');
+const useEffect     = React.useEffect;
+const useRef        = React.useRef;
+const PropTypes     = require('prop-types');
+const ChatMessage   = require('./ChatMessage');
+
+
+const ConferenceChat = (props) => {
+    const messagesEndRef = useRef(null);
+
+    const scrollToBottom = () => {
+        messagesEndRef.current.scrollIntoView({behavior: 'smooth'})
+    }
+
+    useEffect(scrollToBottom, [props.messages, props.scroll]);
+
+    let prevMessage = null;
+    const entries = props.messages.filter((message) => {
+        return !message.content.startsWith('?OTRv')
+    }).map((message, idx) => {
+        let continues = false;
+        if (prevMessage !== null  && prevMessage.sender.uri == message.sender.uri) {
+            continues = true;
+        }
+        prevMessage = message;
+        return (
+            <ChatMessage key={idx} message={message} cont={continues} />
+        )
+    });
+
+    return (
+        <div className="drawer-chat">
+            {entries}
+            <div ref={messagesEndRef} />
+        </div>
+    );
+};
+
+ConferenceChat.propTypes = {
+    scroll: PropTypes.bool,
+    messages: PropTypes.array.isRequired
+};
+
+
+module.exports = ConferenceChat;

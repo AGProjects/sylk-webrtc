f6d5e870be1cc832c1b4ca9abdcdfdc78e7af25c
Tijmen de Mes <tijmen@ag-projects.com>
2021-06-10 13:47:49 +0000

    Added contact list component

---
+++ src/app/components/Chat/ContactList.js
@@ -0,0 +1,403 @@
+'use strict';
+
+const React             = require('react');
+const {
+    useEffect,
+    useState,
+    useRef
+} = React;
+const debug             = require('debug');
+const PropTypes         = require('prop-types');
+const xss               = require('xss');
+const { DateTime }      = require('luxon');
+const { makeStyles }    = require('@material-ui/core/styles');
+const { 
+    ListSubheader,
+    List, 
+    ListItem, 
+    ListItemAvatar,
+    ListItemSecondaryAction,
+    ListItemText,
+    IconButton,
+    Grid,
+    Typography,
+    Divider,
+    Menu,
+    MenuItem,
+    Chip
+} = require('@material-ui/core');
+const { 
+    Lock: LockIcon, 
+    Done: DoneIcon, 
+    DoneAll: DoneAllIcon 
+} = require('@material-ui/icons');
+
+const UserIcon = require('../UserIcon');
+
+const DEBUG = debug('blinkrtc:ContactList');
+
+
+const formatTime = (message) => {
+    const date = DateTime.fromJSDate(message.timestamp);
+    const diff = date.diff(DateTime.local().startOf('day'), 'days').as('days');
+    const yearDiff = date.diff(DateTime.local().startOf('year'), 'year').as('year');
+
+    if (yearDiff >= 1) {
+        return date.toFormat('dd/LL/yyyy');
+    }
+    if (diff < -6) {
+        return date.toFormat('dd/LL');
+    }
+    if (diff < -1) {
+        return date.toFormat('EEEE');
+    }
+    if (diff < 0) {
+        return 'Yesterday';
+    }
+    if (diff < 1) {
+        return date.toFormat('HH:mm');
+    }
+    return date.toFormat('dd/LL');
+};
+
+
+const styleSheet = makeStyles((theme) => ({
+    header: {
+        fontSize: '17px',
+        fontFamily: 'inherit'
+    },
+    date: {
+        fontSize: '14px',
+        fontFamily: 'inherit',
+        color: '#888'
+    },
+    root: {
+        fontSize: '14px',
+        fontFamily: 'inherit',
+        whiteSpace: 'nowrap',
+        overflow: 'hidden',
+        textOverflow: 'ellipsis'
+    },
+    selected: {
+        '&:before': {
+            backgroundColor: '#EBEBEB',
+            position: 'absolute',
+            top: 0,
+            bottom: 0,
+            left: -15,
+            right: -15,
+            zIndex: -1,
+            content: '""'
+        }
+    },
+    listItem: {
+        '&:before': {
+            backgroundColor: '#fff',
+            position: 'absolute',
+            top: 0,
+            bottom: 0,
+            left: -15,
+            right: -15,
+            zIndex: -1,
+            content: '""'
+        }
+    },
+    subheader: {
+        fontFamily: 'inherit',
+        fontSize: 16,
+        textTransform: 'uppercase',
+        height: '66px',
+        lineHeight: '66px',
+        color: '#333'
+    },
+    scrollable: {
+        overflow: 'auto',
+        marginRight: -15,
+        paddingRight: 15,
+        scrollbarWidth: 'thin'
+    },
+    chipSmall: {
+        height: 18,
+        fontSize: 11
+    },
+    iconSmall: {
+        width: 12,
+        height: 12
+    },
+    doneIcon: {
+        fontSize: 15,
+        verticalAlign: 'middle',
+        color: 'green'
+    },
+    item: {
+        fontSize: '14px',
+        fontFamily: 'inherit',
+        color: '#333',
+        minHeight: 0,
+    }
+}));
+
+const ContactList = (props) => {
+    const classes = styleSheet(props);
+    const [filteredMessages, setFilteredMessages] = useState([]);
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const contactRef = React.useRef(null);
+
+    useEffect(() => {
+        if (props.filter !== '') {
+            setFilteredMessages(
+                [].concat.apply(
+                    [],
+                    Object.values(props.messages)
+                ).filter(contact =>
+                    contact.content.indexOf(props.filter) !== -1 && contact.contentType !== 'text/rsa-public-key'
+                )
+            );
+        } else {
+            setFilteredMessages([]);
+        }
+    }, [props.filter, props.messages]);
+
+
+    const getDisplayName = React.useCallback((uri) => {
+        if (props.contactCache.has(uri)) {
+            return {uri: uri, displayName: props.contactCache.get(uri)};
+        }
+        return {uri: uri};
+    }, [props.contactCache]);
+
+    const contacts = React.useMemo(() => {
+        const OrderedKeys = [];
+        let newItem = null;
+        Object.keys(props.messages).filter(name => {
+            let identity = getDisplayName(name);
+            return (identity.displayName && identity.displayName.toLowerCase().indexOf(props.filter.toLowerCase()) !== -1)
+                || name.indexOf(props.filter) !== -1
+        }).forEach(key => {
+            if (props.messages[key] && props.messages[key].length) {
+                let lastItem = props.messages[key][props.messages[key].length - 1]
+                OrderedKeys.push(Object.assign({message: lastItem}, getDisplayName(key)));
+            } else {
+                newItem = key;
+            }
+        });
+        OrderedKeys.sort((a,b) => b.message.timestamp - a.message.timestamp);
+        if (newItem !== null) {
+            OrderedKeys.unshift({uri: newItem});
+        }
+        return OrderedKeys;
+    }, [props.messages, props.filter, getDisplayName]);
+
+
+    const statusIcon = (message) => {
+        const state = message.state;
+        if (state === 'accepted') {
+            return (<DoneIcon style={{color: '#888'}} className={classes.doneIcon}/>);
+        }
+        if (state === 'delivered') {
+            return (<DoneIcon className={classes.doneIcon}/>);
+        }
+        if (state === 'displayed') {
+            return (<DoneAllIcon className={classes.doneIcon} />);
+        }
+    };
+
+    const parseContent = (message) => {
+        const contentType = message.contentType;
+        if (contentType === 'text/html') {
+            const content = xss(message.content, {
+                whiteList: [], // empty, means filter out all tags
+                stripIgnoreTag: true, // filter out all HTML not in the whitelist
+                stripIgnoreTagBody: ['script'] // the script tag is a special case, we need
+                // to filter out its content
+            });
+            return content;
+        } else if (contentType === 'text/plain') {
+            return message.content;
+        } else if (contentType === 'text/rsa-public-key') {
+            return (
+                <Chip
+                    component = "span"
+                    classes = {{sizeSmall: classes.chipSmall, iconSmall: classes.iconSmall}}
+                    variant = "outlined"
+                    size  = "small"
+                    icon  = {<LockIcon />}
+                    label = "Public key"
+                />
+            );
+        }
+    };
+
+
+    const getHighlightedText = (text, highlight) =>{
+        // Split on highlight term and include term into parts, ignore case
+        if (text === undefined) {
+            return;
+        }
+        const parts = text.split(new RegExp(`(${highlight})`, 'gi'));
+        return (
+            <span>
+                {parts.map((part, i) =>
+                    (
+                        <span key={i} style={part.toLowerCase() === highlight.toLowerCase() ? { fontWeight: 'bold' } : {} }>
+                            {part}
+                        </span>
+                    )
+                )}
+            </span>
+        );
+    }
+
+    const switchChat = (uri, id) => {
+        props.loadMessages(uri, id);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (<div className={classes.scrollable}>
+        <List disablePadding>
+            {props.filter && props.defaultDomain && Object.keys(props.messages).indexOf(props.filter) === -1 &&
+                [
+                    <ListItem key="new" style={{height: '66px'}}>
+                        Start a new chat to {props.filter.indexOf('@') === -1 ? `${props.filter}@${props.defaultDomain}` : props.filter}
+                        <ListItemSecondaryAction>
+                            <IconButton aria-label="add" edge="end" onClick={props.startChat} className={classes.margin}>
+                                <i className="fa fa-plus fa-2x" />
+                            </IconButton>
+                        </ListItemSecondaryAction>
+                    </ListItem>,
+                    <Divider component="li" key="divider" />
+                ]
+            }
+            {props.filter && contacts.length !== 0 &&
+                <React.Fragment>
+                    <ListSubheader className={classes.subheader}>Chats</ListSubheader>
+                    <Divider component="li" key="divider" variant="inset"/>
+                </React.Fragment>
+            }
+            <Menu
+                anchorEl = {anchorEl}
+                open = {Boolean(anchorEl)}
+                onClose = {handleClose}
+                keepMounted
+            >
+                <MenuItem className={classes.item} onClick={() => {props.removeChat(contactRef.current); handleClose()}}>
+                    Remove Chat
+                </MenuItem>
+            </Menu>
+        {contacts.map(contact => (
+            [
+                <ListItem
+                    className = {props.selectedUri === contact.uri ? classes.selected : classes.listItem}
+                    alignItems = "flex-start"
+                    key = {contact.uri}
+                    onClick = {()=>switchChat(contact.uri)}
+                    onContextMenu = {(event)=>{event.preventDefault(); contactRef.current = contact.uri; setAnchorEl(event.currentTarget);}}
+                    disableGutters
+                >
+                    <ListItemAvatar style={{minWidth: 60, marginTop:0}}>
+                        <UserIcon identity={contact} active={false} chatContact={true}/>
+                    </ListItemAvatar>
+                    <ListItemText
+                        secondaryTypographyProps={{className: classes.root}}
+                        primary={
+                            <React.Fragment>
+                                <Grid container spacing={2} justify="space-between" alignItems="baseline">
+                                    <Grid item>
+                                        <Typography
+                                            variant="h4"
+                                            className={classes.header}
+                                            style={{flexGrow: 1}}
+                                        >
+                                        {getHighlightedText(contact.displayName || contact.uri, props.filter)}
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item>
+                                        <Typography className={classes.date}>
+                                            {contact.message && statusIcon(contact.message)}
+                                            {contact.message && formatTime(contact.message)}
+                                        </Typography>
+                                    </Grid>
+                                </Grid>
+                            </React.Fragment>
+                        }
+                        secondary = {contact.message && parseContent(contact.message)}
+                    />
+                </ListItem>,
+                <Divider component="li" key={`divider_${contact.uri}`}/>
+            ]
+        ))}
+        {props.filter && filteredMessages.length !== 0 && 
+            <React.Fragment>
+                <ListSubheader className={classes.subheader}>Messages</ListSubheader>
+                <Divider component="li" key="divider" />
+                {filteredMessages.map(message => (
+                    [
+                        <ListItem
+                            className = {classes.listItem} alignItems="flex-start"
+                            key = {message.id}
+                            onClick = {()=> {
+                                let group = message.receiver;
+                                if (message.state === 'received') {
+                                    group = message.sender.uri;
+                                }
+                                switchChat(group, message.id)
+                            }}
+                            disableGutters
+                        >
+                            <ListItemText
+                                secondaryTypographyProps = {{className: classes.root}}
+                                primary = {
+                                    <React.Fragment>
+                                        <Grid container spacing={2} justify="space-between" alignItems="baseline">
+                                            <Grid item>
+                                                <Typography
+                                                    variant = "h4"
+                                                    className = {classes.header}
+                                                    style = {{flexGrow: 1}}
+                                                >
+                                                    {message.state === 'received'
+                                                        ? getDisplayName(message.sender.uri, props.contactCache).displayName
+                                                        : (
+                                                            getDisplayName(message.receiver, props.contactCache).displayName
+                                                            || getDisplayName(message.receiver, props.contactCache).uri
+                                                        )
+                                                    }
+                                                </Typography>
+                                            </Grid>
+                                            <Grid item>
+                                                <Typography className={classes.date}>
+                                                    {message && statusIcon(contact.message)}
+                                                    {message && formatTime(message)}
+                                                </Typography>
+                                            </Grid>
+                                        </Grid>
+                                    </React.Fragment>
+                                }
+                                secondary = {message && getHighlightedText(parseContent(message), props.filter)}
+                            />
+                        </ListItem>,
+                        <Divider component="li" key={`divider_${message.id}`}/>
+                    ]
+                ))}
+            </React.Fragment>
+        }
+        </List>
+    </div>);
+}
+
+ContactList.propTypes = {
+    messages      : PropTypes.object.isRequired,
+    contactCache  : PropTypes.object.isRequired,
+    loadMessages  : PropTypes.func.isRequired,
+    startChat     : PropTypes.func.isRequired,
+    removeChat    : PropTypes.func.isRequired,
+    defaultDomain : PropTypes.string.isRequired,
+    filter        : PropTypes.string,
+    selectedUri   : PropTypes.string
+};
+
+
+module.exports = ContactList;

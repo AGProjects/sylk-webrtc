6f97114c49a7477f3cbf51a68a6562b7f1cfc143
Tijmen de Mes <tijmen@ag-projects.com>
2022-02-17 12:04:11 +0000

    Added line chart component

---
+++ src/app/components/Statistics/LineChart.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const React          = require('react');
+const PropTypes      = require('prop-types');
+
+const {
+    ResponsiveContainer,
+    LineChart,
+    Line } = require('recharts');
+
+const SylkLineChart = ({
+    data,
+    dataKey,
+    height,
+    type
+}) => {
+    if (!height) {
+        height = 60
+    }
+    if (!type) {
+        type = 'step';
+    }
+
+    return (
+        <ResponsiveContainer width="100%" height={height}>
+            <LineChart
+                width={150}
+                height={height}
+                data={data}
+                margin={{ top: 5, right: 0, left: 0, bottom: 5 }}
+            >
+                <Line
+                    type={type}
+                    dataKey={dataKey} 
+                    isAnimationActive={false}
+                    stroke="#2e6da4"
+                    dot={false}
+                />
+            </LineChart>
+        </ResponsiveContainer>
+    )
+};
+
+SylkLineChart.propTypes = {
+    data: PropTypes.array.isRequired,
+    dataKey: PropTypes.string.isRequired,
+    height: PropTypes.number,
+    type: PropTypes.string
+};
+
+
+module.exports = SylkLineChart;
+

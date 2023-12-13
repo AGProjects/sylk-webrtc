ba4a034cc98e26b4540d5fd2b34ff5fd04faa4de
Tijmen de Mes <tijmen@ag-projects.com>
2023-12-13 13:30:51 +0000

    Added TabPanel

---
+++ src/app/components/TabPanel.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const React = require('react');
+const PropTypes = require('prop-types');
+
+
+const TabPanel = (props) => {
+    const { children, value, index, ...other } = props;
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`tabpanel-${index}`}
+            aria-labelledby={`tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <div>
+                    {children}
+                </div>
+            )}
+        </div>
+    );
+}
+
+
+TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired
+};
+
+module.exports = TabPanel;

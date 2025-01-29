566e231ffc65956b607949d0a7b8904c55913b89
Tijmen de Mes <tijmen@ag-projects.com>
2025-01-29 17:16:01 +0000

    Updated electron-notarize dependency

--- build/afterSignHook.js
+++ build/afterSignHook.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const fs = require('fs');
 const path = require('path');
-const { notarize } = require('electron-notarize');
+const { notarize } = require('@electron/notarize');
 
 module.exports = async function (params) {
     // Only notarize the app on Mac OS only.
